b4cdf23aba1d4090e4ea8f87c2c34f8305a34a99
Даниил <@LAPTOP-EEPRU07S>
2022-06-07 21:14:56 +0700

    Добавьте файлы проекта.

---
+++ LISSING/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цену за 1 кг = ");
+            double C = double.Parse(Console.ReadLine());
+            Console.Write("Введите вес яблок = ");
+            double V = double.Parse(Console.ReadLine());
+            double x = C * V;
+            Console.WriteLine($"Стоимость яблок = {x}");
+        }
+    }
+}
